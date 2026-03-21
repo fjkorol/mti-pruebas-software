a1a1be9bfe4bfc29f6fba76e80ce085af1b58501
Fernando Korol <fkorol@siu.edu.ar>
2026-03-21 14:57:11 -0300

    Commit inicial con repositorio playwright para pruebas de software

---
+++ tests/carritoCompras.spec.ts
@@ -0,0 +1,44 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://demoblaze.com/');
+  await page.getByRole('link', { name: 'Log in' }).click();
+  await page.locator('#loginusername').click();
+  await page.locator('#loginusername').fill('user_manu_test');
+  await page.locator('#loginpassword').click();
+  await page.locator('#loginpassword').fill('user123');
+  await page.getByRole('button', { name: 'Log in' }).click();
+  await page.getByRole('link', { name: 'Phones' }).click();
+  await page.getByRole('link', { name: 'Nexus' }).click();
+  page.once('dialog', dialog => {
+    console.log(`Dialog message: ${dialog.message()}`);
+    dialog.dismiss().catch(() => {});
+  });
+  await page.getByRole('link', { name: 'Add to cart' }).click();
+  await page.getByRole('link', { name: 'PRODUCT STORE' }).click();
+  await page.getByRole('link', { name: 'Laptops' }).click();
+  await page.getByRole('link', { name: 'MacBook air' }).click();
+  page.once('dialog', dialog => {
+    console.log(`Dialog message: ${dialog.message()}`);
+    dialog.dismiss().catch(() => {});
+  });
+  await page.getByRole('link', { name: 'Add to cart' }).click();
+  await page.getByRole('listitem').filter({ hasText: 'Cart' }).click();
+  await page.getByRole('button', { name: 'Place Order' }).click();
+  await page.getByRole('textbox', { name: 'Total: 1350 Name:' }).click();
+  await page.getByRole('textbox', { name: 'Total: 1350 Name:' }).fill('Fernando');
+  await page.getByRole('textbox', { name: 'Total: 1350 Name:' }).press('Tab');
+  await page.getByRole('textbox', { name: 'Country:' }).fill('Posadas');
+  await page.getByRole('textbox', { name: 'Country:' }).press('Tab');
+  await page.getByRole('textbox', { name: 'City:' }).fill('Misiones');
+  await page.getByRole('textbox', { name: 'City:' }).press('Tab');
+  await page.getByRole('textbox', { name: 'Credit card:' }).click();
+  await page.getByRole('textbox', { name: 'Credit card:' }).fill('123456789');
+  await page.getByRole('textbox', { name: 'Month:' }).click();
+  await page.getByRole('textbox', { name: 'Month:' }).fill('9');
+  await page.getByRole('textbox', { name: 'Year:' }).click();
+  await page.getByRole('textbox', { name: 'Year:' }).fill('2026');
+  await page.getByRole('button', { name: 'Purchase' }).click();
+  await expect(page.locator('body')).toContainText('Thank you for your purchase!');
+
+});
